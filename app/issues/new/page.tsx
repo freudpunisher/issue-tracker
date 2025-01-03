99f83039657ad7e8cfab4c28e935f47c8f195c7c
freudpunisher <punishergte@gmail.com>
2025-01-03 11:09:09 +0200

    `feat: added react-hook-form and updated issue form to use it`

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,15 +4,32 @@ import { Button, TextArea, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import React from 'react'
+import { useForm, Controller} from 'react-hook-form';
+
+interface IssueForm{
+
+    title: string,
+    description: string
+}
+
+
+
+
+
 
 const NewIssuePage = () => {
+    const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className='max-w-xl space-y-5'>
-          <TextField.Root   placeholder="Title" />
-          <SimpleMDE placeholder='put some description' />
+    <form className='max-w-xl space-y-5' onSubmit={handleSubmit(() => console.log("submit"))}>
+          <TextField.Root placeholder="Title"  {...register('title')} />
+          <Controller
+              name="description"
+              control={control}
+              render={({field}) => <SimpleMDE placeholder='put some description' {...field} /> }
+          />
           <Button>Submit Issue</Button>
       
-    </div>
+    </form>
   )
 }
 
