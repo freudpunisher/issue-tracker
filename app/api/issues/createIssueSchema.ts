45de1703f8f4e02bb7c397d65cf04aa65b9e0d49
freudpunisher <punishergte@gmail.com>
2025-01-03 11:53:23 +0200

    `Removed zod schema and import, replaced with import from createIssueSchema file`

---
+++ app/api/issues/createIssueSchema.ts
@@ -0,0 +1,6 @@
+import { z } from "@/node_modules/zod/lib/external";
+
+export const createIssueSchema = z.object({
+    title: z.string().min(1, 'Title is required').max(255),
+    description: z.string().min(1, 'Description is required').max(255),
+});
